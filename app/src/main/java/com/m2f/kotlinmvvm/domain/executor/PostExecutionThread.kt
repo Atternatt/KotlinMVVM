a096ef3de3c12d2d1c84720c5bbfd91a8ab446c2
Marc <ignis.fatue@gmail.com>
2017-04-09 20:34:09 +0200

    added base interactor with flowable

--- app/src/main/java/com/m2f/kotlinmvvm/domain/executor/PostExecutionThread.kt
+++ app/src/main/java/com/m2f/kotlinmvvm/domain/executor/PostExecutionThread.kt
@@ -2,6 +2,10 @@ package com.m2f.kotlinmvvm.domain.executor
 
 import io.reactivex.Scheduler
 
+/**
+ * @author Marc Moreno
+ * @version 1
+ */
 interface PostExecutionThread {
 
     val scheduler: Scheduler
---
+++ app/src/main/java/com/m2f/kotlinmvvm/domain/interactor/BaseInteractor.kt
@@ -0,0 +1,41 @@
+package com.m2f.kotlinmvvm.domain.interactor
+
+import com.m2f.kotlinmvvm.domain.executor.PostExecutionThread
+import io.reactivex.Flowable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.Executor
+
+/**
+ * @author Marc Moreno
+ * @version 1
+ */
+abstract class BaseInteractor<Source, Param>
+constructor(val postExecutionThread: PostExecutionThread, val threadExecutor: Executor) : Disposable {
+
+    val compositeDisposable: CompositeDisposable = CompositeDisposable()
+
+    fun execute(param: Param,
+                onNext: (Source) -> Unit = {},
+                onComplete: () -> Unit = {},
+                onError: (Throwable) -> Unit = {}) {
+        addDisposable(buildFlowable(param)
+                .subscribeOn(Schedulers.from(threadExecutor))
+                .observeOn(postExecutionThread.scheduler).subscribe(onNext, onError, onComplete))
+    }
+
+    abstract fun buildFlowable(param: Param): Flowable<Source>
+
+    fun addDisposable(disposable: Disposable) {
+        compositeDisposable.add(disposable)
+    }
+
+    override fun isDisposed(): Boolean {
+        return compositeDisposable.isDisposed
+    }
+
+    override fun dispose() {
+        compositeDisposable.dispose()
+    }
+}
