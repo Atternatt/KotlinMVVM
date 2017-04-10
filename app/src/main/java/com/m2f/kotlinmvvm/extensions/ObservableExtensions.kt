273748e89383d620ca8630a544af3d22c0e42831
Marc <ignis.fatue@gmail.com>
2017-04-10 13:33:13 +0200

    add first aproach of the result transformer

--- app/src/main/java/com/m2f/kotlinmvvm/extensions/ObservableExtensions.kt
+++ app/src/main/java/com/m2f/kotlinmvvm/extensions/ObservableExtensions.kt
@@ -1,8 +1,17 @@
 package com.m2f.kotlinmvvm.extensions
 
 import com.jakewharton.retrofit2.adapter.rxjava2.Result
+import com.m2f.kotlinmvvm.exceptions.RetryableException
+import com.m2f.kotlinmvvm.exceptions.UnhandledException
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
 import io.reactivex.Observable
-import retrofit2.Response
+import io.reactivex.ObservableSource
+import org.reactivestreams.Publisher
+import java.io.IOException
+import java.net.ConnectException
+import java.util.concurrent.TimeoutException
+
 
 /**
  * @author Marc Moreno
@@ -10,6 +19,56 @@ import retrofit2.Response
  */
 
 
-fun <T> Observable<Result<T>>.extractResult(): Observable<Response<T>> {
-    return this.compose { it.map { it.response() } }
+
+fun <T> T?.propagate(): Observable<T> {
+    return Observable.create { emitter ->
+        if(!emitter.isDisposed) {
+            this?.let { emitter.onNext(it) }
+        }
+    }
+}
+
+fun <T> T?.propagateFlowable(backpressureStrategy: BackpressureStrategy = BackpressureStrategy.LATEST): Flowable<T> {
+    return Flowable.create<T>( { emitter ->
+        if (!emitter.isCancelled) {
+            this?.let { emitter.onNext(it) }
+        }
+    }, backpressureStrategy)
+}
+
+fun <Body> Result<Body>.extractBody(): Body? {
+    return if (this.isError) {
+        with(this.error()) {
+            if (this is IOException) {
+                when (this) {
+                    is ConnectException -> throw RetryableException()
+                    is TimeoutException -> throw RetryableException()
+                    else -> throw UnhandledException(this.toString())
+                }
+            } else {
+                android.util.Log.wtf("UNHANDLED_EXCEPTION", this@extractBody.error())
+                throw UnhandledException(this.toString())
+            }
+        }
+    } else {
+        val response = this.response()
+        if (!response.isSuccessful) {
+            val code: Int = response.code()
+            if (404 == code) {
+                return null
+            } else {
+                throw UnhandledException("http error code: $code")
+            }
+        } else {
+            return response.body()
+        }
+    }
+}
+
+fun <Body> Observable<Result<Body>>.extractResult(): ObservableSource<Body> {
+    return this.compose { it.switchMap { it.extractBody().propagate() } }
+}
+
+fun <Body> Flowable<Result<Body>>.extractResult(): Publisher<Body> {
+    return this.compose { it.switchMap { it.extractBody().propagateFlowable() } }
 }
