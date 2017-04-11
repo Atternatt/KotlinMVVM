7e44e84b43805a8bfcc750044c6b5b157a3fdce5
Marc <ignis.fatue@gmail.com>
2017-04-11 11:10:47 +0200

    added base di component and module for search (dummy)

---
+++ app/src/main/java/com/m2f/kotlinmvvm/presentation/search/SearchComponent.kt
@@ -0,0 +1,16 @@
+package com.m2f.kotlinmvvm.presentation.search
+
+import com.m2f.kotlinmvvm.main.di.ApplicationComponent
+import dagger.Component
+import io.folioapp.android.di.CustomScope
+
+/**
+ * @author Marc Moreno
+ * @version 1
+ */
+@CustomScope
+@Component(dependencies = arrayOf(ApplicationComponent::class), modules = arrayOf(SearchModule::class))
+interface SearchComponent {
+
+    //todo: add the injectable activity
+}
---
+++ app/src/main/java/com/m2f/kotlinmvvm/presentation/search/SearchModule.kt
@@ -0,0 +1,11 @@
+package com.m2f.kotlinmvvm.presentation.search
+
+import dagger.Module
+
+/**
+ * @author Marc Moreno
+ * @version 1
+ */
+@Module
+class SearchModule {
+}
